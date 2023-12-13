e746fd55098aa2cc06d5ad4b0f21bcb00db45bf5
c-susan <susan.chen@stonybrook.edu>
2023-12-13 17:19:19 +0000

    updates

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE geo_area_id (
     id INT AUTO_INCREMENT PRIMARY KEY,
     geo_id INT(10),
-    location_name VARCHAR(255)
+    geo_type_name VARCHAR(255),
+    geo_place_name VARCHAR(255)
 );
-
