4d1f189e3ae85130c4dd0cdad5dfab6a764a8a63
Susan Chen <123512714+c-susan@users.noreply.github.com>
2024-01-06 20:05:35 -0500

    Update migration.sql

--- db/migration.sql
+++ db/migration.sql
@@ -12,4 +12,3 @@ INSERT INTO alembic_version (version_num) VALUES ('537980e0b839');
 -- Running upgrade 537980e0b839 -> 8202909f63ae
 
 UPDATE alembic_version SET version_num='8202909f63ae' WHERE alembic_version.version_num = '537980e0b839';
-
